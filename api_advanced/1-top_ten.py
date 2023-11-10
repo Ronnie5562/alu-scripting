29a1536f254661bd7da7537941e63b97b58e53d7
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-11-11 00:48:50 +0200

    Task 1

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+##!/usr/bin/python3
+import requests
+"""
+Prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+
+def top_ten(subreddit):
+    """Main function"""
+    URL = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception as e:
+        print(None)
