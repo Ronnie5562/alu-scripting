47d6e24a458dd164554bee765dfa58b307f23429
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-11-11 23:53:25 +0200

    Task

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -16,4 +16,6 @@ def top_ten(subreddit):
         HOT_POSTS = RESPONSE.json().get("data").get("children")
         [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception as e:
+        """_summary_
+        """
         print(None)
