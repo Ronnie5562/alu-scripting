ccbeacbde356cca97ee8912fdb0c45501cb73ed7
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-11-12 00:12:36 +0200

    Task

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""0-subs.py"""
+"""Return the number of subscribers of a given subreddit"""
 
 import requests
-
 
 def number_of_subscribers(subreddit):
     """Main function"""
     URL = f"https://www.reddit.com/r/{subreddit}/about.json"
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
 
-    try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+    if RESPONSE.status_code == 200:
         return RESPONSE.json().get("data").get("subscribers")
-    except Exception as e:
+    else:
         return 0
