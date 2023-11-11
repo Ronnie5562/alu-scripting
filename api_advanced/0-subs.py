d46ca5d1aa67a574bec92daf9b361c500ffac867
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-11-11 23:56:34 +0200

    Task

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import requests
+
 """
 Query the Reddit API and return the number of subscribers of a given subreddit
 """
 
+
 def number_of_subscribers(subreddit):
     """Main function"""
     URL = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
+
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import requests
+
 """
 Prints the titles of the first 10 hot posts listed for a given subreddit.
 """
+
 
 def top_ten(subreddit):
     """Main function"""
@@ -10,12 +12,8 @@ def top_ten(subreddit):
 
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        """_summary_
-        """
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
         HOT_POSTS = RESPONSE.json().get("data").get("children")
         [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception as e:
-        """_summary_
-        """
         print(None)
