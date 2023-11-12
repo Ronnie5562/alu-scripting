dc7d6e94e0bee3a97d6112278cef85ed185e1d6c
Ronald Abimbola <r.abimbola@alustudent.com>
2023-11-12 03:32:16 -0800

    Task

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -13,5 +13,5 @@ def top_ten(subreddit):
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
         HOT_POSTS = RESPONSE.json().get("data").get("children")
         [print(post.get('data').get('title')) for post in HOT_POSTS]
-    except Exception as e:
+    except Exception:
         print(None)
--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -18,20 +18,19 @@ def count_words(subreddit, word_list=[], hot_list=[], after=""):
         [hot_list.append(post.get('data').get('title')) for post in HOT_POSTS]
         if after is not None:
             return count_words(subreddit, word_list, hot_list, after)
-        
+
         new_dict = {}
-        word_list = set(word_list)
+        word_list = set([wrd.lower() for wrd in word_list])
         for title in hot_list:
             for word in title.split():
                 if word.lower() in new_dict:
                     new_dict[word.lower()] += 1
                 else:
                     new_dict.update({word.lower(): 1})
-                    
+
         sorted_dict = sorted(new_dict.items(), key=lambda x: (-x[1], x[0]))
         for key, value in sorted_dict:
             if (key in word_list) and (value > 0):
                 print("{}: {}".format(key, value))
-        
-    except Exception as e:
+    except Exception:
         pass
