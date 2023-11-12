5027dada1bf71c493fbe16966e6c0764119b77c7
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-11-12 12:59:56 +0200

    Task

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -17,20 +17,21 @@ def count_words(subreddit, word_list=[], hot_list=[], after=""):
         HOT_POSTS = RESPONSE.json().get("data").get("children")
         [hot_list.append(post.get('data').get('title')) for post in HOT_POSTS]
         if after is not None:
-            return count_words(subreddit, hot_list=hot_list, after=after)
+            return count_words(subreddit, word_list, hot_list=hot_list, after=after)
         
         new_dict = {}
         word_list = set(word_list)
         for title in hot_list:
-            for word in title:
+            for word in title.split():
                 if word.lower() in new_dict:
                     new_dict[word.lower()] += 1
                 else:
                     new_dict.update({word.lower(): 1})
-        
-        for key, value in sorted(new_dict):
+                    
+        sorted_dict = sorted(new_dict.items(), key=lambda x: (-x[1], x[0]))
+        for key, value in sorted_dict:
             if (key in word_list) and (value > 0):
                 print(f"{key}: {value}")
         
-    except Exception:
-        return None
+    except Exception as e:
+        print(e)
